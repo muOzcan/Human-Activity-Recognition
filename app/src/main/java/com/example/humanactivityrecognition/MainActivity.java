8b6b0104403f1d9589cfcd8c13e458212f64bea9
mustafa özcan <mustafa_ozcan_@hotmail.com>
2023-12-19 05:24:36 +0300

    X,Y,Z values of Accelerometer showed in graph with GraphView Library

--- app/src/main/java/com/example/humanactivityrecognition/MainActivity.java
+++ app/src/main/java/com/example/humanactivityrecognition/MainActivity.java
@@ -1,6 +1,6 @@
 package com.example.humanactivityrecognition;
 
-import androidx.appcompat.app.AppCompatActivity;
+
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -8,64 +8,118 @@ import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Handler;
+import android.os.Message;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+
+
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
-    TextView tv1,tv2,tv3,tv4,tv5,tv6;
     private SensorManager mSensorManager;
-    private Sensor accelerometer;
-    private Sensor gyroscope;
+    private LineGraphSeries<DataPoint> seriesAccX,seriesAccY,seriesAccZ;
+    private static double currentX;
+    private LineGraphSeries<DataPoint> seriesGyroX,seriesGyroY,seriesGyroZ;
+    private ThreadPoolExecutor liveChartExecutor;
+    private LinkedBlockingQueue<Double> accelerationQueue = new LinkedBlockingQueue<>(10);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Değerleri ekranda göstermek için text tanımlaması
-        tv1=findViewById(R.id.tv1);
-        tv2=findViewById(R.id.tv2);
-        tv3=findViewById(R.id.tv3);
-        tv4=findViewById(R.id.tv4);
-        tv5=findViewById(R.id.tv5);
-        tv6=findViewById(R.id.tv6);
-        // SensorManageri al
-        mSensorManager= (SensorManager) getSystemService(SENSOR_SERVICE);
-        // Accelerometer sensörünü al
-        accelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        // Gyroscope sensörünü al
-        gyroscope = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
-        // Accelerometer için dinleme
-        if (accelerometer != null){
-            mSensorManager.registerListener(MainActivity.this,accelerometer,mSensorManager.SENSOR_DELAY_UI);
-        }
-        // Gyroscope için dinleme
-        if (gyroscope != null){
-            mSensorManager.registerListener(this, gyroscope, mSensorManager.SENSOR_DELAY_UI);
-        }
+
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        GraphView graph = (GraphView) findViewById(R.id.graphAccelerometer);
+
+
+        seriesAccX = new LineGraphSeries<>();
+        seriesAccX.setColor(Color.RED);
+        graph.addSeries(seriesAccX);
+
+        seriesAccY = new LineGraphSeries<>();
+        seriesAccY.setColor(Color.GREEN);
+        graph.addSeries(seriesAccY);
+
+        seriesAccZ = new LineGraphSeries<>();
+        seriesAccZ.setColor(Color.BLUE);
+        graph.addSeries(seriesAccZ);
+
+
+        // activate horizontal zooming and scrolling
+        graph.getViewport().setScalable(true);
+
+        // activate horizontal scrolling
+        graph.getViewport().setScrollable(true);
+
+        // activate horizontal and vertical zooming and scrolling
+        graph.getViewport().setScalableY(true);
+
+        // activate vertical scrolling
+        graph.getViewport().setScrollableY(true);
+        // To set a fixed manual viewport use this:
+        // set manual X bounds
+        graph.getViewport().setXAxisBoundsManual(true);
+        graph.getViewport().setMinX(0.5);
+        graph.getViewport().setMaxX(6.5);
+
+        // set manual Y bounds
+        graph.getViewport().setYAxisBoundsManual(true);
+        graph.getViewport().setMinY(0);
+        graph.getViewport().setMaxY(10);
+
+        currentX = 0;
+
+        // Start chart thread
+        liveChartExecutor = (ThreadPoolExecutor) Executors.newFixedThreadPool(1);
+        if (liveChartExecutor != null)
+            liveChartExecutor.execute(new AccelerationChart(new AccelerationChartHandler()));
+
     }
 
     @Override
-    public void onSensorChanged(SensorEvent event) {
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this,
+                mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_NORMAL);
+    }
 
-        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-            // Ivmeölcer değerleri burada alınır
-            float x = event.values[0];
-            float y = event.values[1];
-            float z = event.values[2];
 
-            tv1.setText("Acc X = " + x);
-            tv2.setText("Acc Y = " + y);
-            tv3.setText("Acc Z = " + z);
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
 
-        } else if (event.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
-            // Gyroskop değerleri burada alınır
-            float x = event.values[0];
-            float y = event.values[1];
-            float z = event.values[2];
-
-            // Gyroskop değerlerini ayrı TextView'lara güncelleyin
-            tv4.setText("Gyro X = " + event.values[0]);
-            tv5.setText("Gyro Y = " + event.values[1]);
-            tv6.setText("Gyro Z = " + event.values[2]);
+        if (sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            getAccelerometer(sensorEvent);
         }
+
+//        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+//            // Ivmeölcer değerleri burada alınır
+//            float x = event.values[0];
+//            float y = event.values[1];
+//            float z = event.values[2];
+//
+//            tv1.setText("Acc X = " + x);
+//            tv2.setText("Acc Y = " + y);
+//            tv3.setText("Acc Z = " + z);
+//
+//        } else if (event.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
+//            // Gyroskop değerleri burada alınır
+//            float x = event.values[0];
+//            float y = event.values[1];
+//            float z = event.values[2];
+//
+//            tv4.setText("Gyro X = " + event.values[0]);
+//            tv5.setText("Gyro Y = " + event.values[1]);
+//            tv6.setText("Gyro Z = " + event.values[2]);
+//        }
 
     }
 
@@ -73,4 +127,77 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     public void onAccuracyChanged(Sensor sensor, int i) {
 
         }
+
+    @Override
+    protected void onPause() {
+        // unregister listener
+        super.onPause();
+        mSensorManager.unregisterListener(this);
     }
+
+    private void getAccelerometer(SensorEvent event) {
+        float[] values = event.values;
+        // Movement
+        double x = values[0];
+        double y = values[1];
+        double z = values[2];
+
+        double accelerationSquareRoot = (x * x + y * y + z * z) / (SensorManager.GRAVITY_EARTH * SensorManager.GRAVITY_EARTH);
+        double acceleration = Math.sqrt(accelerationSquareRoot);
+
+        accelerationQueue.offer(acceleration);
+
+        seriesAccX.appendData(new DataPoint(currentX, x), true, 10);
+        seriesAccY.appendData(new DataPoint(currentX, y), true, 10);
+        seriesAccZ.appendData(new DataPoint(currentX, z), true, 10);
+    }
+
+    private class AccelerationChartHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            Double accelerationY = 0.0D;
+            if (!msg.getData().getString("ACCELERATION_VALUE").equals(null) && !msg.getData().getString("ACCELERATION_VALUE").equals("null")) {
+                accelerationY = (Double.parseDouble(msg.getData().getString("ACCELERATION_VALUE")));
+            }
+
+            seriesAccX.appendData(new DataPoint(currentX, accelerationY), true, 10);
+            currentX = currentX + 1;
+        }
+    }
+
+    private class AccelerationChart implements Runnable {
+        private boolean drawChart = true;
+        private Handler handler;
+
+        public AccelerationChart(Handler handler) {
+            this.handler = handler;
+        }
+
+        @Override
+        public void run() {
+            while (drawChart) {
+                Double accelerationY;
+                try {
+                    Thread.sleep(300); // Speed up the X axis
+                    accelerationY = accelerationQueue.poll();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    continue;
+                }
+                if (accelerationY == null)
+                    continue;
+
+                // currentX value will be exceed the limit of double type range
+                // To overcome this problem comment of this line
+                // currentX = (System.currentTimeMillis() / 1000) * 8 + 0.6;
+
+                Message msgObj = handler.obtainMessage();
+                Bundle b = new Bundle();
+                b.putString("ACCELERATION_VALUE", String.valueOf(accelerationY));
+                msgObj.setData(b);
+                handler.sendMessage(msgObj);
+            }
+        }
+    }
+}
+
